108dff55696ff13b36f2b5627c9d254f0fd0825f
amrit kumar <amritx8@gmail.com>
2021-06-11 12:02:33 +0530

    Admin Menu Completed

--- CRS-JEDI-G4-Flipkart-POS/src/com/flipkart/application/CRSAdminMenu.java
+++ CRS-JEDI-G4-Flipkart-POS/src/com/flipkart/application/CRSAdminMenu.java
@@ -3,6 +3,7 @@ package com.flipkart.application;
 import java.util.Scanner;
 
 import com.flipkart.bean.Course;
+import com.flipkart.bean.Professor;
 
 public class CRSAdminMenu {
 	
@@ -61,15 +62,47 @@ public class CRSAdminMenu {
 						System.out.print("Enter Course professor:");
 					    takeInput = sc.next();
 					    newCourse.setDescriptions(takeInput);
+					    
 					case 2:
 					
 					case 3:
-					
-					case 4:
-					
-					case 5:
+						Professor newProfessor = new Professor();
 						
-					default;
+						String takeInput2;
+						
+						System.out.print("Enter Professor Id:");
+					    int ID = sc.nextInt();
+					    newProfessor.setUserID(ID);
+
+						System.out.print("Enter Professor Name:");
+					    takeInput2 = sc.next();
+					    newProfessor.setName(takeInput2);
+
+						System.out.print("Enter Professor Email:");
+					    takeInput2 = sc.next();
+					    newProfessor.setEmail(takeInput2);
+
+						System.out.print("Enter Professor Password:");
+					    takeInput2 = sc.next();
+					    newProfessor.setPassword(takeInput2);
+
+						System.out.print("Enter Professor EmployeeID:");
+					    takeInput2 = sc.next();
+					    newProfessor.setEmpID(takeInput2);
+					    
+					    System.out.print("Enter Professor Department:");
+					    takeInput2 = sc.next();
+					    newProfessor.setDept(takeInput2);
+						
+					case 4:
+						
+						
+					case 5:
+						System.out.println("Logged out\n");
+						return;
+						
+					default:
+						System.out.println("Invalid choice");
 				}
 			}
 		} catch(Exception e) {
