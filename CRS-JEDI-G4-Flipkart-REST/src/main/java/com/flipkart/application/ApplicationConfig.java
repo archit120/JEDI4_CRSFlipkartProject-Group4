c56ab61e934dd1b6540e35a0242c74e69a3ced86
archit120 <architrungta120@gmail.com>
2021-06-15 21:14:20 +0530

    logger+api fix

--- CRS-JEDI-G4-Flipkart-REST/src/main/java/com/flipkart/application/ApplicationConfig.java
+++ CRS-JEDI-G4-Flipkart-REST/src/main/java/com/flipkart/application/ApplicationConfig.java
@@ -7,12 +7,14 @@ package com.flipkart.application;
 //import com.flipkart.restController.CommonRestAPI;
 //import com.flipkart.restController.ProfessorRestAPI;
 import com.flipkart.restController.StudentRestAPI;
+import org.apache.log4j.BasicConfigurator;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ApplicationConfig extends ResourceConfig {
 	
 	public ApplicationConfig() {
 		// Register all the services over here.
+		BasicConfigurator.configure();
 
 //		register(AdminRestAPI.class);
 //		register(CommonRestAPI.class);
