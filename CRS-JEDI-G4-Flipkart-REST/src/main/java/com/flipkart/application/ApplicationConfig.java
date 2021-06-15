110dd3f734d0b0151f6ac4c9d31b5f01de65e9b5
amrit kumar <amritx8@gmail.com>
2021-06-15 19:00:50 +0530

    Rest setup completed

--- CRS-JEDI-G4-Flipkart-REST/src/main/java/com/flipkart/application/ApplicationConfig.java
+++ CRS-JEDI-G4-Flipkart-REST/src/main/java/com/flipkart/application/ApplicationConfig.java
@@ -4,9 +4,9 @@
 package com.flipkart.application;
 
 import com.flipkart.restController.AdminRestAPI;
-import com.flipkart.restController.CommonRestAPI;
-import com.flipkart.restController.ProfessorRestAPI;
-import com.flipkart.restController.StudentRestAPI;
+//import com.flipkart.restController.CommonRestAPI;
+//import com.flipkart.restController.ProfessorRestAPI;
+//import com.flipkart.restController.StudentRestAPI;
 import org.glassfish.jersey.server.ResourceConfig;
 
 public class ApplicationConfig extends ResourceConfig {
@@ -15,8 +15,8 @@ public class ApplicationConfig extends ResourceConfig {
 		// Register all the services over here.
 
 		register(AdminRestAPI.class);
-		register(CommonRestAPI.class);
-		register(ProfessorRestAPI.class);
-		register(StudentRestAPI.class);
+//		register(CommonRestAPI.class);
+//		register(ProfessorRestAPI.class);
+//		register(StudentRestAPI.class);
 	}
 }
