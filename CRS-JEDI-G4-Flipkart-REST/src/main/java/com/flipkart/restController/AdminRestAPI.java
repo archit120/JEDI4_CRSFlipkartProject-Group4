4067f4b4401ed6db268e83f22031f357a3b62884
archit120 <architrungta120@gmail.com>
2021-06-16 09:57:05 +0530

    revert rest changes

--- CRS-JEDI-G4-Flipkart-REST/src/main/java/com/flipkart/restController/AdminRestAPI.java
+++ CRS-JEDI-G4-Flipkart-REST/src/main/java/com/flipkart/restController/AdminRestAPI.java
@@ -1,112 +1,85 @@
+/**
+ * 
+ */
 package com.flipkart.restController;
-import com.flipkart.bean.*;
-import com.flipkart.exception.LoginFailedException;
-import com.flipkart.service.AdminImpl;
-import com.flipkart.service.CourseImpl;
-import com.flipkart.service.StudentImpl;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.crypto.URIReferenceException;
 
 /**
- * @author amrit
+ * @author anike
  *
  */
-@Path("/adminApi")
+@Path("/customerApi")
 public class AdminRestAPI {
-
+	
+	// GET (Fetch) Implementation.
 	@GET
-	@Path("/login")
+	@Path("/customer")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response login(@QueryParam("username") String username, @QueryParam("password") String password) throws LoginFailedException {
+	public String getCustomerDetails() {
+		
+		//  We have to call all the services in the Rest layer with the respective methods.
+		//  client --- service ---- dao ----> SQL
+       
+//		Customer customer=new Customer();
+//		customer.setCustomerId(101);
+//		customer.setCustomerName("ABC");
+//		customer.setCustomerAddress("Delhi");
+//
+//	   return customer;
 
-		try {
-			return Response.status(200).entity((new AdminImpl()).login(username, password)).build();
-		}
-		catch (LoginFailedException e) {
-			return Response.status(403).entity(e).build();
-		}
-		catch(Exception e) {
-			return Response.status(500).entity(e).build();
-		}
+		return "Hello World!";
 	}
+//
+//	@POST
+//	@Path("/post")
+//	@Consumes("application/json")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response createTrackInJSON(Customer customer) {
+//        System.out.println("hit post service");
+//
+//        System.out.println("value of cid from UI " +customer.getCustomerId());
+//        System.out.println("value of cname form UI" +customer.getCustomerName());
+//
+//        // Put customer details in service call
+//        String result = "customer saved : " + customer;
+//
+//
+//		return Response.status(201).entity(result).build();
+//
+//	}
+//
+//
+//
+//	@PUT
+//	@Path("/update")
+//	@Consumes("application/json")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Customer updateCustomer(Customer customer){
+//
+//		// rest of impl write here in code
+//
+//		customer.setCustomerName(customer.getCustomerName() +"updated");
+//		return customer;
+//
+//	}
+//
+//	@DELETE
+//	@Path("/delete/{customerId}")
+//	public Response deleteCustomer(@PathParam("customerId") int customerId)
+//	throws URIReferenceException{
+//
+//		// service query to perfomr the delete operation
+//
+//		// implementation
+//		return Response.status(200).entity("Track id " +customerId +
+//				"successfully deleted").build();
+//
+//
+//	}
+//
 
-	@POST
-	@Path("/addCourse")
-	@Consumes("application/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addCourse(@QueryParam("courseCode") String courseCode, @QueryParam("department") String department, @QueryParam("descriptions") String descriptions, @QueryParam("preRequisites") String preRequisites) {
-
-		Course newCourse = new Course();
-
-		newCourse.setCourseCode(courseCode);
-		newCourse.setDepartment(department);
-		newCourse.setDescriptions(descriptions);
-		newCourse.setPreRequisites(preRequisites);
-		newCourse.setCourseCatalogueId(1);
-
-		try {
-			return Response.status(200).entity((new CourseImpl()).addCourse(newCourse)).build();
-		}
-		catch (Exception e) {
-			return Response.status(400).entity(e).build();
-		}
-	}
-
-	@DELETE
-	@Path("/removeCourse")
-	@Consumes("application/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response removeCourse(@QueryParam("courseCode") int courseCatalogueID, @QueryParam("courseCode") String courseCode) {
-
-		// service query to perform the delete operation
-		// implementation
-
-		try {
-			CourseCatalogue current = new CourseCatalogue();
-			current.setId(courseCatalogueID);
-
-			return Response.status(200).entity((new CourseImpl().removeCourse(current, courseCode))).build();
-		} catch (Exception e) {
-
-			return Response.status(400).entity(e).build();
-		}
-	}
-
-	@POST
-	@Path("/addProfessor")
-	@Consumes("application/json")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addProfessor(@QueryParam("username") String username, @QueryParam("name") String name, @QueryParam("email") String email, @QueryParam("password") String password, @QueryParam("password") String empID, @QueryParam("department") String department) {
-
-		Professor newProfessor = new Professor();
-
-		newProfessor.setUsername(username);
-		newProfessor.setName(name);
-		newProfessor.setEmail(email);
-		newProfessor.setPassword(password);
-		newProfessor.setEmpID(empID);
-		newProfessor.setDept(department);
-
-		try {
-			return Response.status(200).entity((new AdminImpl().addProfessor(newProfessor))).build();
-		}
-		catch (Exception e) {
-			return Response.status(400).entity(e).build();
-		}
-	}
-
-	@GET
-	@Path("/getStudentListWaitingForApproval")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getStudentListWaitingForApproval() {
-
-		try {
-			return Response.status(200).entity(new StudentImpl().getStudentsWaitingApprocal()).build();
-		}
-		catch (Exception e) {
-			return Response.status(400).entity(e).build();
-		}
-	}
 }
